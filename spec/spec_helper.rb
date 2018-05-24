4aa5b0d39881fdd9065195342b369ab562dd713c
kun.liu <kun.liu@dji.com>
2018-05-24 10:55:23 +0800

    fix empty lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ SimpleCov.start
 require 'bundler/setup'
 require 'jaeger/client'
 
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
